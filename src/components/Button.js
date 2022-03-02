3f89c37bbf2d57c88f69f76c6dda34897bb8037a
Michal Jan Warecki <mwarecki1@gmail.com>
2022-03-02 20:15:17 +0100

    Fix caption display bug in "Button"

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,7 +11,7 @@ import React from "react";
 export default function (props) {
   let caption_visible = props.visible === undefined ? true : props.visible;
   let visible_classes = !caption_visible ? "visually-hidden visually-hidden-focusable" : null;
-  let caption = <span className={visible_classes}>props.caption</span>;
+  let caption = <span className={visible_classes}>{props.caption}</span>;
   let icon = props.icon === undefined ? null : <i className={`bi bi-${props.icon}`}/>;
   let classes = "btn ms-1";
   if (props.classNames !== undefined) {
