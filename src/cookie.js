36844a0f05b0d1bdc314bc94827ee17b5723d637
Michal Jan Warecki <mwarecki1@gmail.com>
2022-03-03 18:03:41 +0100

    Better getCookie + docstrings. Docstrings for setCookie.

--- src/cookie.js
+++ src/cookie.js
@@ -1,13 +1,29 @@
+/**
+ *
+ * @param name {string}
+ * @returns {string|undefined}
+ */
 export default function getCookie(name) {
   let cookies = document.cookie;
   let split = cookies.split(name + "=");
   if (split.length === 1) {
     return undefined;
   }
-
+  if (split[0] !== "") {
+    split = cookies.split("; " + name + "=");
+  }
+  if (split.length === 1) {
+    return undefined;
+  }
   return split[1].split(';')[0]
 }
 
+/**
+ *
+ * @param name {string} cookie name
+ * @param value {string} cookie value
+ * @param duration {number} days duration
+ */
 export function setCookie(name, value, duration=1) {
   let now = new Date();
   now.setTime(now.getTime() + (duration*24*3600*1000));
