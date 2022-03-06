95579a8b95954b0e6cdf93e4685119d9166a938b
Michal Jan Warecki <mwarecki1@gmail.com>
2022-03-06 14:13:44 +0100

    State handling for login and register actions (#7).

--- src/components/register.js
+++ src/components/register.js
@@ -5,8 +5,16 @@ export default class RegisterView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      login_errors: null,
-      register_errors: null,
+      login_message: {
+        display: false,
+        success: false,
+        message: null,
+      },
+      register_message: {
+        display: false,
+        success: false,
+        message: null,
+      },
       register_passwords_matching: true,
       registered: false,
     }
@@ -14,6 +22,15 @@ export default class RegisterView extends Component {
 
     this.login=this.login.bind(this);
     this.register=this.register.bind(this);
+
+    this.alert_messages = {
+      1: "Brukarnamnet eller passordet er feil.",
+      2: "Du er no registrert og kan logge inn.",
+      3: "Passorda er ikkje lik.",
+      4: "Du må godta vilkåra for å bli registrert",
+      5: "Dette brukarnamnet er opptatt. Ville du kanskje logge inn?",
+      6: "Ukjent feil. Venlegast ta kontakt med brukarstøtte.",
+    }
   }
 
   async login() {
@@ -33,18 +50,36 @@ export default class RegisterView extends Component {
           'x-csrftoken': this.csrftoken
         }
       })
+    console.log(response.headers);
     let json = await response.json();
     console.log(json);
 
     if (response.status === 200) {
-      this.props.login_completion(json.username, json.access_token)
+      this.props.login_completion(json.username, json.access_token);
       return;
     }
 
     if (json.error !== undefined) {
       console.log(json.error);
-      this.setState({login_errors: json.error})
+      this.setState(
+        {
+          login_message: {
+            display: true,
+            success: false,
+            message: 1,
+          }
+        }
+      );
+      return;
     }
+
+    this.setState({
+      login_message: {
+        display: true,
+        success: false,
+        message: 6,
+      }
+    });
   }
 
   async register() {
@@ -53,12 +88,20 @@ export default class RegisterView extends Component {
     let password2 = document.querySelector("#register-repeat-password").value;
     let email = document.querySelector("#register-email").value;
 
+    // Check if passwords are the same.
     if (password !== password2) {
-      this.setState({register_passwords_matching: false});
+      this.setState(
+        {
+          register_passwords_matching: false,
+          register_message: {
+            display: true,
+            success: false,
+            message: 3,
+          }
+        }
+      );
       console.log("Passwords not matching");
       return;
-    } else {
-      this.setState({register_passwords_matching: true});
     }
 
     let formData = new FormData();
@@ -74,9 +117,19 @@ export default class RegisterView extends Component {
         headers: {
           'x-csrftoken': this.csrftoken
         }
-      })
+      }
+      );
     if (response.status === 200) {
-      this.setState()
+      this.setState(
+        {
+          register_passwords_matching: true,
+          register_message: {
+            display: true,
+            success: true,
+            message: 2,
+          }
+        }
+        );
       return;
     }
 
@@ -84,16 +137,44 @@ export default class RegisterView extends Component {
     console.log(json);
     if (json.error !== undefined) {
       console.log(json.error);
-      this.setState({login_errors: json.error})
+      this.setState(
+        {
+          register_passwords_matching: true,
+          register_message: {
+            display: true,
+            success: false,
+            message: 5,
+          }
+        }
+        );
+      return;
     }
+
+    this.setState(
+      {
+        register_passwords_matching: true,
+        register_message: {
+          display: true,
+          success: false,
+          message: 6,
+        }
+      }
+    )
   }
 
   render() {
-    let login_errors = this.state.login_errors === null ? null : <span className="is-invalid">Innlogging mislykkast!</span>
-    let register_errors = this.state.register_errors === null ? null : <span className="is-invalid">Dette brukarnamnet er opptatt.</span>
-    let registered = this.state.registered ? <span>Takk, kan no logge inn i feltet til venstre.</span> : null;
-    return <div className="row container">
-      <div className="col-md-6">
+    let login_errors = this.state.login_message.display
+      ? <div className={"alert " + (this.state.login_message.success ? "alert-success" : "alert-danger")}>
+          { this.alert_messages[this.state.login_message.message] }
+        </div>
+      : null;
+    let register_errors = this.state.register_message.display
+      ? <div className={"alert " + (this.state.register_message.success ? "alert-success" : "alert-danger")}>
+          { this.alert_messages[this.state.register_message.message] }
+        </div>
+      : null;
+    return <div className="row container-fluid gy-3 m-0">
+      <div className="col-sm-6">
         <h3>Logg inn</h3>
         <form>
           { login_errors }
@@ -108,10 +189,9 @@ export default class RegisterView extends Component {
           <div onClick={this.login} className="btn btn-primary">Logg inn!</div>
         </form>
       </div>
-      <div className="col-md-6">
+      <div className="col-sm-6">
         <h3>Registrer ny brukar</h3>
         <form>
-          { registered }
           { register_errors }
           <div>
             <label htmlFor="register-username" className="form-label">Brukarnamn</label>
